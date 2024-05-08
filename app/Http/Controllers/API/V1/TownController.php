7d72e00fc8fc5167b5a9d3eea00bdd087562a9e4
Olivier ZINSOU <zinsouoliviers@gmail.com>
2024-05-08 10:46:05 +0100

    Add town model, form requests, seeder, migration, controller, policy and factory

---
+++ app/Http/Controllers/API/V1/TownController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTownRequest;
+use App\Http\Requests\UpdateTownRequest;
+use App\Models\Town;
+
+class TownController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $towns = Town::all();
+
+        return response()->json($towns);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Town $town)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Town $town)
+    {
+        //
+    }
+}
---
+++ database/seeders/TownSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Town;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TownSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Town::factory(5)->create();
+    }
+}
