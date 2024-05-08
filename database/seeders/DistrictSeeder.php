c50136c7c7cf5724caabf85c92d987fdb05a4cae
Olivier ZINSOU <zinsouoliviers@gmail.com>
2024-05-08 10:42:16 +0100

    Add district model, form requests, policy, factory, migration, controller and seeder

---
+++ database/seeders/DistrictSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\District;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        District::factory(5)->create();
+    }
+}
