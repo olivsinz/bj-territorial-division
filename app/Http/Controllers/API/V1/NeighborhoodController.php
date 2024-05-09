993081d07e3b43fe83b40a504cebc7dab2f46b19
Olivier ZINSOU <zinsouoliviers@gmail.com>
2024-05-09 13:45:20 +0100

    Add pagination

--- app/Http/Controllers/API/V1/NeighborhoodController.php
+++ app/Http/Controllers/API/V1/NeighborhoodController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Neighborhood;
+use Illuminate\Http\Request;
 
 class NeighborhoodController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $neighborhoods = Neighborhood::all();
+        $page = $request->input('page', 1);
+        $pageSize = $request->input('page_size', 20);
+
+        $neighborhoods = Neighborhood::paginate($pageSize, ['*'], 'page', $page);
 
         return response()->json($neighborhoods);
     }
