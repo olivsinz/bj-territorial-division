b69b8c4b2043eed8e4cce517b246158684048b6f
Olivier ZINSOU <zinsouoliviers@gmail.com>
2024-05-10 16:36:50 +0100

    Add caching for departments' neighborhoods

--- app/Http/Controllers/API/V1/Departments/DepartmentNeighborhoodController.php
+++ app/Http/Controllers/API/V1/Departments/DepartmentNeighborhoodController.php
@@ -14,14 +14,19 @@ class DepartmentNeighborhoodController extends Controller
      */
     public function index(Department $department): JsonResponse
     {
-        $neighborhoods = DB::table('neighborhoods')
-            ->select('neighborhoods.name')
-            ->join('districts', 'districts.id', '=', 'neighborhoods.district_id')
-            ->join('towns', 'towns.id', '=', 'districts.town_id')
-            ->join('departments', 'departments.id', '=', 'towns.department_id')
-            ->where('departments.name', $department->name)
-            ->get();
+        $neighborhoods = cache()->remember('departments.'.$department->name.'.neighborhoods', now()->addMonths(6), function () use ($department) {
+            return DB::table('neighborhoods')
+                ->select('neighborhoods.name')
+                ->join('districts', 'districts.id', '=', 'neighborhoods.district_id')
+                ->join('towns', 'towns.id', '=', 'districts.town_id')
+                ->join('departments', 'departments.id', '=', 'towns.department_id')
+                ->where('departments.name', $department->name)
+                ->get();
+        });
 
-        return response()->json(['department' => $department->name, 'neighborhoods' => $neighborhoods]);
+        return response()->json([
+            'department' => $department->name, 
+            'neighborhoods' => $neighborhoods
+        ]);
     }
 }
