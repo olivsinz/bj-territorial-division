4bada11ac3cb18cc64bcd46dfa69c87b272b9df1
Olivier ZINSOU <zinsouoliviers@gmail.com>
2025-01-29 14:20:47 +0100

    wip

--- app/Http/Controllers/API/V1/NeighborhoodController.php
+++ app/Http/Controllers/API/V1/NeighborhoodController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NeighborhoodCollection;
 use App\Models\Neighborhood;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NeighborhoodController extends Controller
@@ -12,15 +12,27 @@ class NeighborhoodController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request): JsonResponse
+    public function index(Request $request): NeighborhoodCollection
     {
-        $neighborhoods = Neighborhood::paginate(
-            $request->integer('page_size', 20),
-            ['*'],
-            'page',
-            $request->integer('page', 1)
-        );
+        // $neighborhoods = Neighborhood::paginate(
+        //     $request->integer('page_size', 20),
+        //     ['*'],
+        //     'page',
+        //     $request->integer('page', 1)
+        // );
 
-        return response()->json($neighborhoods);
+        $pageSize = $request->integer('page_size', 20);
+        $page = $request->integer('page', 1);
+
+        return cache()->remember('neighborhoods.page-size-' . $pageSize . '.page-' . $page, 100, function () use ($pageSize, $page) {
+            return new NeighborhoodCollection(Neighborhood::paginate(
+                $pageSize,
+                ['*'],
+                'page',
+                $page
+            ));
+        });
+
+        // return response()->json($neighborhoods);
     }
 }
---
+++ tests/Feature/ArchitectureTest.php
@@ -0,0 +1,129 @@
+<?php
+
+uses()->group('architecture');
+
+// Globals
+test('ensure that global function like "dd" is not used')
+    ->expect('dd')
+    ->not->toBeUsed();
+
+test('ensure that global function like "dump" is not used')
+    ->expect('dump')
+    ->not->toBeUsed();
+
+test('ensure that global function like "die" is not used')
+    ->expect('die')
+    ->not->toBeUsed();
+
+// Strict types
+test('ensure all files within App\Services use strict types')
+    ->expect('App\Services')
+    ->toUseStrictTypes();
+
+// arch('ensure provided namespaces here use strict types')
+//     ->expect([
+//         'App\Builders',
+//         'App\Enums',
+//         'App\Faker',
+//         'App\Http\Resources',
+//         'App\Traits',
+//         'App\ValueObjects',
+//     ])
+//     ->toUseStrictTypes()
+//     ->ignoring(['App\Traits\HandleFiles']);
+
+// // Enums
+// test('ensure that all files within App\Enums namespace are enums')
+//     ->expect('App\Enums')
+//     ->toBeEnums()
+//     ->ignoring(['App\Enums\Concerns', 'App\Enums\Attributes']);
+
+// // Traits
+// test('ensure that all files within App\Traits namespace are traits')
+//     ->expect('App\Traits')
+//     ->toBeTraits();
+
+// App\Actions
+test('ensure all files within App\Actions are classes')
+    ->expect('App\Actions')
+    ->toBeClasses();
+
+// App\Rules
+test('ensure all files within App\Rules implement "Illuminate\Contracts\Validation\ValidationRule"')
+    ->expect('App\Rules')
+    ->toImplement('Illuminate\Contracts\Validation\ValidationRule');
+
+// App\Models
+test('ensure all files within App\Models are classes')
+    ->expect('App\Models')
+    ->toBeClasses();
+
+test('ensure that globals functions like request() are not used within App\Models namespace')
+    ->expect('request')
+    ->not->toBeUsedIn('App\Models');
+
+// App\Services
+test('ensure all files within App\Services are classes')
+    ->expect('App\Services')
+    ->toBeClasses();
+
+test('ensure that globals functions like request() are not used within App\Services namespace')
+    ->expect('request')
+    ->not->toBeUsedIn('App\Services');
+
+test('ensure that globals functions like response() are not used within App\Services namespace')
+    ->expect('response')
+    ->not->toBeUsedIn('App\Services');
+
+test('ensure that Illuminate\Http\Request classes are not used in App\Services namespace')
+    ->expect('Illuminate\Http\Request')
+    ->not->toBeUsedIn('App\Services');
+
+test('ensure that all files within App\Services namespace have suffix "Service"')
+    ->expect('App\Services')
+    ->toHaveSuffix('Service');
+
+// Controllers
+test('ensure that all files within App\Http\Controllers are classes')
+    ->expect('App\Http\Controllers')
+    ->toBeClasses();
+
+test('ensure that all files within App\Http\Controllers have suffix "Controller"')
+    ->expect('App\Http\Controllers')
+    ->toHaveSuffix('Controller');
+
+test('ensure all files within App\Http\Controllers extend "App\Http\Controllers\Controller"')
+    ->expect('App\Http\Controllers')
+    ->toExtend("App\Http\Controllers\Controller");
+
+// Requests
+test('ensure that all files within App\Http\Requests have suffix "Request"')
+    ->expect('App\Http\Requests')
+    ->toHaveSuffix('Request');
+
+test('ensure all files within App\Http\Requests extend "Illuminate\Foundation\Http\FormRequest"')
+    ->expect('App\Http\Requests')
+    ->toExtend("Illuminate\Foundation\Http\FormRequest");
+
+// App\Http\Middleware
+test('ensure all files within App\Http\Middleware are classes')
+    ->expect('App\Http\Middleware')
+    ->toBeClasses();
+
+// Providers
+test('ensure that all files within App\Providers namespace have suffix "Provider"')
+    ->expect('App\Providers')
+    ->toHaveSuffix('Provider');
+
+test('ensure all files within App\Providers extend "Illuminate\Support\ServiceProvider"')
+    ->expect('App\Providers')
+    ->toExtend("Illuminate\Support\ServiceProvider");
+
+// Databases
+test('ensure that all files within Database\Factories have suffix "Factory"')
+    ->expect('Database\Factories')
+    ->toHaveSuffix('Factory');
+
+test('ensure that all files within Database\Seeders have suffix "Seeder"')
+    ->expect('Database\Seeders')
+    ->toHaveSuffix('Seeder');
