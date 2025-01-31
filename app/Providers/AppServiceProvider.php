b05a98be0643f80602c55b87bb30958cb5d111f9
Olivier ZINSOU <zinsouoliviers@gmail.com>
2025-01-31 19:51:13 +0100

    database state loading after migrations end

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Console\Commands\EnsureDatabaseStateIsLoaded;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\MigrationsEnded;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
@@ -39,6 +43,20 @@ class AppServiceProvider extends ServiceProvider
 
                 info("Attempted to lazy load [{$relation}] on model [{$class}].");
             });
+
+            Event::listen(
+                MigrationsEnded::class,
+                function (MigrationsEnded $event) {
+                    // down() method run during a rollback action. When rolling back,
+                    // tables underlined by our state commands will not exist because they
+                    // will be removed furing roll back.
+                    if ($event->method === 'down') {
+                        return;
+                    }
+
+                    Artisan::call(EnsureDatabaseStateIsLoaded::COMMAND_SIGNATURE);
+                }
+            );
         }
     }
 
