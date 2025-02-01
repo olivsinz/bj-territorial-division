cfb2c561ebd53cc0744622c581e7628d6857f5b8
Olivier ZINSOU <zinsouoliviers@gmail.com>
2025-02-01 22:24:26 +0100

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
@@ -25,6 +26,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('telescope:prune --hours=48')->daily();
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->shouldRenderJsonWhen(function ($request, \Throwable $error) {
+            return $request->expectsJson() || $request->is('api/*');
+        });
+
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
@@ -41,15 +46,21 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
 
+        // $exceptions->render(function (\Throwable $e, $request) {
+        //     if ($e instanceof HttpException && $e->getStatusCode() === 500) {
+        //         return response()->json([
+        //             'message' => 'An unexpected error occurred. Please try again later.',
+        //         ], 500);
+        //     }
+
+        //     return null;
+        // });
+
         $exceptions->render(function (ServiceUnavailableHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
                     'message' => 'Service currently unavailable.',
                 ], 503);
             }
-        });
-
-        $exceptions->shouldRenderJsonWhen(function ($request, \Throwable $error) {
-            return $request->expectsJson() || $request->is('api/*');
         });
     })->create();
